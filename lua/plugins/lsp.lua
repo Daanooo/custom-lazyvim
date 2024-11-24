ed31f85403d36d220aa148ea62b00f4180578df1
Daanooo <73757140+Daanooo@users.noreply.github.com>
2024-11-24 16:50:48 +0100

    added formatting and treesitter parser for go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,31 @@
 return {
+  -- All lsp servers and lspconfig stuff
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         pyright = {},
         gopls = {},
+      },
+    },
+  },
+
+  -- Treesitter config
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "go",
+      },
+    },
+  },
+
+  -- Formatters
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports", "gofmt" },
       },
     },
   },
