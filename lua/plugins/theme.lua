4220d068d497756231b7c7527dff8795797b4f4c
Daanooo <73757140+Daanooo@users.noreply.github.com>
2025-03-09 12:56:37 +0100

    Install extra color schemas

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,13 @@
 return {
-  { "sainnhe/everforest" },
+  { "sainnhe/everforest", lazy = false },
+  { "rose-pine/neovim", name = "rose-pine", lazy = false },
+  { "AlexvZyl/nordic.nvim", lazy = false },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "moonfly",
     },
   },
 }
